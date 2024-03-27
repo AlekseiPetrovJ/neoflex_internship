cd9aa965aecf5f4b475591c0288a5652ed8036e3
Aleksei Petrov <peta0982@gmail.com>
2024-03-27 21:30:12 +0500

    add test calculate vacation pay with start date (holiday don't  paid)

--- src/test/java/ru/petrov/service/VacationPayServiceTest.java
+++ src/test/java/ru/petrov/service/VacationPayServiceTest.java
@@ -7,19 +7,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import ru.petrov.util.exception.DurationVacationNotCorrectException;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class VacationPayServiceTest {
-    private final Environment env;
     private final VacationPayService vacationPayService;
     private final int minDays;
     private final int maxDays;
     private final double daysPerYear;
+    private final double averageSalary = 100.5;
+    private final int vacationDays = 10;
 
     @Autowired
     public VacationPayServiceTest(Environment env, VacationPayService vacationPayService) {
-        this.env = env;
         this.vacationPayService = vacationPayService;
         minDays = Integer.parseInt(env.getProperty("salary-settings.min-days-vacation"));
         maxDays = Integer.parseInt(env.getProperty("salary-settings.max-days-vacation"));
@@ -29,8 +31,6 @@ class VacationPayServiceTest {
     @Test
     @DisplayName("Расчет отпускных при корректных входных данных")
     void calculateVacationPay() {
-        double averageSalary = 100.5;
-        int vacationDays = 10;
         double expectedPay = averageSalary * vacationDays / daysPerYear;
         double actualPay = vacationPayService.calculateVacationPay(averageSalary, vacationDays);
         assertEquals(expectedPay, actualPay, 0.01);
@@ -53,6 +53,22 @@ class VacationPayServiceTest {
                 vacationPayService.calculateVacationPay(100.0, maxDays + 1));
 
         assertTrue(thrown.getMessage().contains("maximum"));
+    }
 
+    @Test
+    @DisplayName("Расчет отпускных с корректными входными данными и указанием даты начала отпуска. " +
+            "На отпуск не приходятся праздничные дни")
+    void calculateVacationPayWithStartDate(){
+        double expectedPay = averageSalary * vacationDays / daysPerYear;
+        double actualPay = vacationPayService.calculateVacationPay(averageSalary, vacationDays, LocalDate.parse("2024-02-01"));
+        assertEquals(expectedPay, actualPay, 0.01);
+    }
+    @Test
+    @DisplayName("Расчет отпускных с корректными входными данными и указанием даты начала отпуска. " +
+            "На отпуск приходится праздничный день")
+    void calculateVacationPayWithStartDateAndHolidays(){
+        double expectedPay = averageSalary * (vacationDays -1) / daysPerYear;
+        double actualPay = vacationPayService.calculateVacationPay(averageSalary, vacationDays, LocalDate.parse("2024-02-15"));
+        assertEquals(expectedPay, actualPay, 0.01);
     }
 }
